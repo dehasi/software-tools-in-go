1711983523042801178609ba79e35b61e4a9c7fe
Ravil Galeyev <dehasi@yandex.ru>
2024-03-24 17:38:14 +0100

    CH-4

--- ch4/sorting/sort.go
+++ ch4/sorting/sort.go
@@ -1,6 +1,6 @@
 package sorting
 
-// BubbleSort -- sorts input arr in place
+// BubbleSort -- sorts input arr in place using bubble sort
 func BubbleSort(arr []int) {
 	n := len(arr)
 	for i := 0; i < n; i++ {
