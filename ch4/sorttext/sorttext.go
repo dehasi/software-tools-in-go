42634e561b1e4a59b05e957a4baa47adaabafe58
Ravil Galeyev <dehasi@yandex.ru>
2024-03-24 22:42:27 +0100

    CH-4

--- ch4/sorttext/sorttext.go
+++ ch4/sorttext/sorttext.go
@@ -12,8 +12,24 @@ func inmemsort() {
 	linebuf := make([]string, MAXLINES)
 	nlines := gtext(linebuf, STDIN)
 	if nlines > 0 {
-		// shell(posbuf)
+		shell(linebuf, nlines)
 		ptext(linebuf, nlines, STDOUT)
+	}
+}
+
+func shell(linebuf []string, nlines int) {
+	for gap := nlines / 2; gap > 0; gap /= 2 {
+		for i := gap; i < nlines; i++ {
+			for j := i - gap; j >= 0; j = j - gap {
+				jq := j + gap
+				if linebuf[j] <= linebuf[jq] {
+					break
+				}
+				tmp := linebuf[j]
+				linebuf[j] = linebuf[jq]
+				linebuf[jq] = tmp
+			}
+		}
 	}
 }
 
