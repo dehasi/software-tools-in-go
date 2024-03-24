f863a04efe4d6fb81444e086a50ecec0aa96a101
Ravil Galeyev <dehasi@yandex.ru>
2024-03-24 22:40:12 +0100

    CH-4

--- ch4/sorttext/sorttext.go
+++ ch4/sorttext/sorttext.go
@@ -9,30 +9,29 @@ const MAXLINES = 300
 
 func inmemsort() {
 
-	posbuf := make([]string, MAXLINES)
-	nlines := gtext(posbuf, STDIN)
+	linebuf := make([]string, MAXLINES)
+	nlines := gtext(linebuf, STDIN)
 	if nlines > 0 {
 		// shell(posbuf)
-		ptext(posbuf, nlines, STDOUT)
+		ptext(linebuf, nlines, STDOUT)
 	}
 }
 
 func ptext(strings []string, nlines int, fd *os.File) {
-	println("ptext, n=" + itoc(nlines))
 	for i := 0; i < nlines; i++ {
 		putstr(strings[i], fd)
 		putcf(NEWLINE, fd)
 	}
 }
 
-func gtext(posbuf []string, fd *os.File) int {
+func gtext(linebuf []string, fd *os.File) int {
 	i := 0
 	for {
 		line, got := getlinef(fd, MAXCHARS)
 		if !got {
 			break
 		}
-		posbuf[i] = line
+		linebuf[i] = line
 		i += 1
 	}
 	return i
