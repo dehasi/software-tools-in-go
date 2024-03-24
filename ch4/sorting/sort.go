71f91788be1b61657427eb4c4690c9040a1dfd63
Ravil Galeyev <dehasi@yandex.ru>
2024-03-24 18:29:09 +0100

    CH-4

--- ch4/sorting/sort.go
+++ ch4/sorting/sort.go
@@ -3,6 +3,20 @@ package sorting
 // BubbleSort -- sorts input arr in place using bubble sort
 func BubbleSort(arr []int) {
 	n := len(arr)
+	for i := n - 1; i > 0; i-- {
+		for j := 0; j < i; j++ {
+			if arr[j] > arr[j+1] {
+				tmp := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = tmp
+			}
+		}
+	}
+}
+
+// ShellSort -- sorts input arr in place using Shell's sort
+func ShellSort(arr []int) {
+	n := len(arr)
 	for i := 0; i < n; i++ {
 		for j := i + 1; j < n; j++ {
 			if arr[i] > arr[j] {
