0931452fbbece6eec65fd2371f1e3dea18254e69
Ravil Galeyev <dehasi@yandex.ru>
2024-03-24 22:32:19 +0100

    CH-4

--- ch4/sorttext/sorttext.go
+++ ch4/sorttext/sorttext.go
@@ -1,5 +1,18 @@
 package main
 
+const MAXCHARS = 10_000
+const MAXLINES = 300
+
+func inmemsort() {
+
+	posbuf := make([]string, MAXLINES)
+	res := gtext(posbuf, STDIN)
+	if res {
+		shell(posbuf)
+		ptext(posbuf, STDOUT)
+	}
+}
+
 func main() {
 
 }
