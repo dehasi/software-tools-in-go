adbfd719b17e7d03304d0e45bf97303e06d6b9b9
Ravil Galeyev <dehasi@yandex.ru>
2024-03-24 22:35:31 +0100

    CH-4

--- ch4/sorttext/sorttext.go
+++ ch4/sorttext/sorttext.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"os"
+)
+
 const MAXCHARS = 10_000
 const MAXLINES = 300
 
@@ -8,9 +12,30 @@ func inmemsort() {
 	posbuf := make([]string, MAXLINES)
 	res := gtext(posbuf, STDIN)
 	if res {
-		shell(posbuf)
+		// shell(posbuf)
 		ptext(posbuf, STDOUT)
 	}
+}
+
+func ptext(strings []string, fd *os.File) {
+	n := len(strings)
+	for i := 0; i < n; i++ {
+		putstr(strings[i], fd)
+		putcf(NEWLINE, fd)
+	}
+}
+
+func gtext(posbuf []string, fd *os.File) bool {
+	i := 0
+	for {
+		line, got := getlinef(fd, MAXCHARS)
+		if !got {
+			break
+		}
+		posbuf[i] = line
+		i += 1
+	}
+	return true
 }
 
 func main() {
