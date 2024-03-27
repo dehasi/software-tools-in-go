8d85367d3cf05e83eb488719b9d69e05228851b3
Ravil Galeyev <dehasi@yandex.ru>
2024-03-27 18:26:34 +0100

    CH-4

--- ch4/bigfiles/qsort.go
+++ ch4/bigfiles/qsort.go
@@ -1,36 +1,70 @@
 package main
 
+const ENDSTR uint8 = 10 // Go dosn't have '\0' concept as C, let's use '\n'
+
 // quick -- quicksort for lines
-func quick(linepos []*string, nlines int, linebuf []string) {
-	rquick(linepos, 0, nlines-1)
+func quick(linepos []int, nlines int, linebuf []uint8) {
+	rquick(0, nlines-1, linepos, linebuf)
 }
 
 // rquick -- recursive quicksort
-func rquick(linepos []*string, lo int, hi int) {
-	if lo >= hi {
-		return
-	}
-	i := lo
-	j := hi
-	piv := *linepos[hi]
-	for i < j {
-		for i < j && *linepos[i] <= piv {
-			i++
+func rquick(lo int, hi int, linepos []int, linebuf []uint8) {
+	if lo < hi {
+		i := lo
+		j := hi
+		pivline := linepos[j] // pivot line
+		for /*repeat*/ {
+			for i < j && cmp(linepos[i], pivline, linebuf) <= 0 {
+				i += 1
+			}
+			for j > i && cmp(linepos[j], pivline, linebuf) >= 0 {
+				j -= 1
+			}
+			if i < j { // out of order pair
+				exchange(linepos, i, j)
+			}
+			if /*until*/ i >= j {
+				break
+			}
 		}
-		for i < j && *linepos[j] >= piv {
-			j--
-		}
-		if i < j {
-			tmp := linepos[j]
-			linepos[j] = linepos[i]
-			linepos[i] = tmp
-		}
-	}
-	// move pivot to i
-	tmp := linepos[hi]
-	linepos[hi] = linepos[i]
-	linepos[i] = tmp
+		// move pivot to i
+		exchange(linepos, i, hi)
 
-	rquick(linepos, lo, i-1)
-	rquick(linepos, i+1, hi)
+		// I don't understand this optimization
+		if (i - lo) < (hi - i) {
+			rquick(lo, i-1, linepos, linebuf)
+			rquick(i+1, hi, linepos, linebuf)
+		} else {
+			rquick(i+1, hi, linepos, linebuf)
+			rquick(lo, i-1, linepos, linebuf)
+		}
+	}
+}
+
+// cmp -- compare linebuf[lp1] with linebuf[lp2]
+func cmp(i int, j int, linebuf []uint8) int {
+	for linebuf[i] == linebuf[j] &&
+		linebuf[i] != ENDSTR {
+		i = i + 1
+		j = j + 1
+	}
+	// get the first not equal symbol
+	if linebuf[i] == linebuf[j] {
+		return 0
+	} else if linebuf[i] == ENDSTR { // 1st is shorter
+		return -1
+	} else if linebuf[j] == ENDSTR { // 2st is shorter
+		return 1
+	} else if linebuf[i] < linebuf[j] {
+		return -1
+	} else {
+		return 1
+	}
+}
+
+// exchange -- exchanges linepos[lp1] with linepos[lp2]; divergents from the original
+func exchange(linepos []int, lp1 int, lp2 int) {
+	tmp := linepos[lp1]
+	linepos[lp1] = linepos[lp2]
+	linepos[lp2] = tmp
 }
